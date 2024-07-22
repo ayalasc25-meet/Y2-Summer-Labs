726c3cbe7a7b24dda077a9a4abddee4ea4e55c69
ayalasc25-meet <ayalayschwartz@gmail.com>
2024-07-22 08:43:36 +0300

    Finished lab

--- Authentication-Lab/app.py
+++ Authentication-Lab/app.py
@@ -3,7 +3,7 @@ from flask import session as login_session
 import pyrebase
 
 
-app = Flask(__name__, templates_folder='templates', static_folder = 'static')
+app = Flask(__name__, template_folder='templates', static_folder = 'static')
 app.config['SECRET_KEY'] = "PASSWORD"
 
 firebaseConfig = {
@@ -20,7 +20,9 @@ firebaseConfig = {
 
   'appId': "1:1088116653430:web:d4b6dc18c5f0a1342a8e57",
 
-  'measurementId': "G-YD5X1BD7HL"
+  'measurementId': "G-YD5X1BD7HL",
+
+  'databaseURL': ""
 
 }
 
@@ -31,16 +33,47 @@ auth = firebase.auth()
 def signup():
     if request.method == 'GET':
         return render_template("signup.html") 
-    else: 
+    else:
         email = request.form['email']
         password = request.form['password']
         try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            login_session['quotes']=[]
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            login_session['quotes'] = []
             return redirect(url_for('home'))
         except:
-            error = "Didn't work, sorry"
-            return render_template("login.html", error=error)
+            error_msg = "Womp it failed. Try again"
+            return render_template("signup.html",error=error_msg)
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template("home.html")
+        
+    login_session["quotes"].append(request.form["quote"])
+    login_session.modified = True
+    print(login_session["quotes"])
+    return redirect(url_for("thanks"))
+        
+
+
+@app.route('/thanks', methods=["GET", "POST"])
+def thanks():
+        return render_template("thanks.html") 
+
+    
+@app.route('/display', methods=["GET", "POST"])
+def display():
+    if request.method == 'GET':
+        return render_tempTruelate("display.html", quotes=login_session["quotes"]) 
+  
+
+@app.route('/signout', methods=["GET", "POST"])
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
+
 
 @app.route('/signin', methods=["GET", "POST"])
 def signin():
@@ -50,34 +83,12 @@ def signin():
         email = request.form['email']
         password = request.form['password']
         try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-        	login_session['quotes']=[]
-            return redirect(url_for('home'))
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            login_session['quotes']=[]
+            return render_template("home.html") 
         except:
             error = "Didn't work, sorry"
             return render_template("signup.html",error=error)
-        
-@app.route('/thanks', methods=["GET", "POST"])
-def thanks():
-    if request.method == 'GET':
-        return render_template("thanks.html") 
-    
-
-@app.route('/display', methods=["GET", "POST"])
-def display():
-    if request.method == 'GET':
-        return render_template("display.html") 
-  
-
-
-@app.route('/home', methods=["GET", "POST"])
-def home():
-    if request.method == "GET":
-        return render_template("home.html")
-    else:
-        login_session['user'] = None
-        auth.current_user = None
-        return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
